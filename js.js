bbae03b20e9a0426ec98d9882904563927eea0c1
KautilyaDK <155764912+KautilyaDK@users.noreply.github.com>
2024-01-05 23:33:43 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const climateCraftButton = document.querySelector('.climatecraft');
+
+    climateCraftButton.addEventListener('click', function() {
+        
+        
+        
+        
+        window.open('https://darklord286.github.io/climate/', '_blank');
+    });
+
+    const pythonButton = document.querySelector('.polyglot');
+
+    pythonButton.addEventListener('click', function() {
+        
+        
+        
+        
+        window.open('https://www.python.org/', '_blank');
+    });
+
+    const valorantButton = document.querySelector('.valorant');
+
+    valorantButton.addEventListener('click', function() {
+        
+        
+        
+        
+        window.open('https://playvalorant.com/en-us/', '_blank');
+    });
+});
+
+
